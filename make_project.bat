61de03006dee34362cc0933bed1a6c7aecec30d3
BLC\Niels <n@osomi.net>
2018-09-07 13:33:21 +0200

    [make_project] Now uses correct settings

--- make_project.bat
+++ make_project.bat
@@ -6,15 +6,18 @@ xcopy "%~dp0CMakeLists.project.txt" "%cd%/CMakeLists.txt*" /y >nul
 
 if not exist "%cd%/app" mkdir "%cd%/app"
 xcopy "%~dp0app" "%cd%/app" /i /y /s >nul
-xcopy "%~dp0app/CMakeLists.project.txt" "%cd%/app/CMakeLists.txt*" /y >nul
+del "%cd%\app\CMakeLists.txt" >nul
+ren "%cd%\app\CMakeLists.project.txt" "CMakeLists.txt" >nul
 
 if not exist "%cd%/app_android" mkdir "%cd%/app_android"
 xcopy "%~dp0app_android" "%cd%/app_android" /i /y /s >nul
-xcopy "%~dp0app_android/CMakeLists.project.txt" "%cd%/app_android/CMakeLists.txt*" /y >nul
+del "%cd%\app_android\CMakeLists.txt" >nul
+ren "%cd%\app_android\CMakeLists.project.txt" "CMakeLists.txt" >nul
 
 if not exist "%cd%/app_pc" mkdir "%cd%/app_pc"
 xcopy "%~dp0app_pc" "%cd%/app_pc" /i /y /s >nul
-xcopy "%~dp0app_pc/CMakeLists.project.txt" "%cd%/app_pc/CMakeLists.txt*" /y >nul
+del "%cd%\app_pc\CMakeLists.txt" >nul
+ren "%cd%\app_pc\CMakeLists.project.txt" "CMakeLists.txt" >nul
 
 xcopy "%~dp0clean.bat" "%cd%/clean.bat*" /y >nul
 xcopy "%~dp0reload.bat" "%cd%/reload.bat*" /y >nul
